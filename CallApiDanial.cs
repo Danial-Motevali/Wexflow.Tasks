4e19b8c2c89415e6b50160f6b3f2768953abeca8
Danial Motevali <danial.motevali.82@gmail.com>
2024-05-07 09:46:05 +0330

    Add project files.

---
+++ CallApiDanial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Wexflow.Core;
+
+namespace Wexflow.Tasks.CallApiDanial
+{
+    public class CallApiDanial : Core.Task
+    {
+        public CallApiDanial(XElement ex, Workflow wf) : base(ex, wf)
+        {
+
+        }
+
+        public override Core.TaskStatus Run()
+        {
+            try
+            {
+                string url = this.GetSetting("Address");
+                string method = this.GetSetting("Method");
+
+                if (url == null || method == null)
+                    return new Core.TaskStatus(Status.Error);
+
+                if(method == "GET")
+                {
+                    var clinent = new HttpClient();
+                    var response = clinent.GetAsync(url);
+
+                    return new Core.TaskStatus(Status.Success);
+                }
+
+
+                return new Core.TaskStatus(Status.Success);
+            }catch (ThreadAbortException)
+            {
+                throw;
+            }
+        }
+    }
+}
